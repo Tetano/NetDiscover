2cac39232a450e7430020f2baba5f261de30c2cc
VINICIUS VICCO PETRARCA DE ASSIS MIRANDA <vinicius.miranda@tre-to.jus.br>
2020-05-08 18:00:39 -0300

    Atualizações de layout simples + Testes

--- app/src/main/java/com/unitins/netdiscover/MainActivity.kt
+++ app/src/main/java/com/unitins/netdiscover/MainActivity.kt
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var btnLogin = findViewById(R.id.btnLogin) as Button
-    var edtLogin = findViewById(R.id.edtEmail) as EditText
-    var edtSenha = findViewById(R.id.edtSenha) as EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var btnLogin = findViewById(R.id.btnLogin) as Button
+        var edtLogin = findViewById(R.id.edtEmail) as EditText
+        var edtSenha = findViewById(R.id.edtSenha) as EditText
         btnLogin.setOnClickListener(){
 
+            if(btnLogin.text.toString().equals("vinicius")){
+                Toast.makeText(this,"Bottão pressionado",Toast.LENGTH_LONG).show();
+            }
+
         }
+
+
     }
 
 
